eba499c235b311c4b7eba86e2662bf750ce9df8f
Ali <alinaqvi514@hotmail.com>
2022-08-13 17:21:15 +0500

    created dtaabase connection and with new mongodb

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
-
+ // "mongodb+srv://blockit:82go3l80k0CVP0QZ@cluster0.uxqbofq.mongodb.net/?retryWrites=true&w=majority",
 const connectDB = async () => {
   try {
     await mongoose.connect(
-        "mongodb://adminB:jHgSsVNInACwzebL@cluster0-shard-00-00.cj2aq.mongodb.net:27017,cluster0-shard-00-01.cj2aq.mongodb.net:27017,cluster0-shard-00-02.cj2aq.mongodb.net:27017/blockitt?ssl=true&replicaSet=atlas-mbd5qc-shard-0&authSource=admin&retryWrites=true&w=majority",
+      
+      "mongodb://adminB:jHgSsVNInACwzebL@cluster0-shard-00-00.cj2aq.mongodb.net:27017,cluster0-shard-00-01.cj2aq.mongodb.net:27017,cluster0-shard-00-02.cj2aq.mongodb.net:27017/blockitt?ssl=true&replicaSet=atlas-mbd5qc-shard-0&authSource=admin&retryWrites=true&w=majority",
+       
       {
         useNewUrlParser: true
       }
